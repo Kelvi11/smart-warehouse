e7b8521b27a2cd7766a674b4f170162bf5576cd2
Kelvin <kelvindervishllari@gmail.com>
2022-06-18 15:30:24 +0200

    init commit

---
+++ src/test/java/com/kelvin/smartwarehouse/SmartWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.smartwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
