91d2b9ea76f39e7cca1e43436aadad54b8051fc5
kelvin-atis <kelvin.dervishllari@atis.al>
2022-07-01 16:00:12 +0200

    exception handling for EntityWithIdNotFoundException and InvalidParameterException

---
+++ src/main/java/com/kelvin/smartwarehouse/exception/ApiException.java
@@ -0,0 +1,23 @@
+package com.kelvin.smartwarehouse.exception;
+
+import java.time.ZonedDateTime;
+
+public class ApiException {
+
+    private final String message;
+    private final ZonedDateTime timestamp;
+
+    public ApiException(String message,
+                        ZonedDateTime timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+}
